cfcf5be3f19bf0b383f57e5f467aad00af387cba
Henry <adefemiadelaja@gmail.com>
2025-09-19 15:54:39 +0100

    Update package.json and actions.ts

--- auth.config.ts
+++ auth.config.ts
@@ -21,6 +21,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
