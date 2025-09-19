6539ae904b23b8315ab73cdcb947644624744a49
Henry <adefemiadelaja@gmail.com>
2025-09-19 15:00:35 +0100

    Update package.json and actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -114,9 +114,9 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    console.error('Database Error:', error);
+    throw new Error('Failed to Delete Invoice.');
   }
 }
 
