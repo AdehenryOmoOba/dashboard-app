cf9681b6fae86729525b9310b8460cf2e8eed69a
Henry <adefemiadelaja@gmail.com>
2025-09-19 14:51:20 +0100

    Completed mvp 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import HideNextBadge from '@/app/ui/hide-next-badge';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
